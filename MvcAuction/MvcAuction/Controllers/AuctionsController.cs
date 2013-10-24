ec0faa27a7f1d4baaa35e872f211f02dd13ac591
jhurant <jhurant@whatswhat.com>
2013-10-24 13:53:16 -0400

    Added a highly typed list view for three sample auctions
    this showed how you can pass a model easily and create a view with a
    single click more or less.

--- MvcAuction/MvcAuction/Controllers/AuctionsController.cs
+++ MvcAuction/MvcAuction/Controllers/AuctionsController.cs
@@ -13,7 +13,36 @@ namespace MvcAuction.Controllers
 
         public ActionResult Index()
         {
-            return View();
+            var auctions = new[] {
+                new Models.Auction()
+                {
+                    Title = "Example Auction #1",
+                    Description = "This is an example Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #2",
+                    Description = "This is a second Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = 30m,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #3",
+                    Description = "This is a third Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 10.00m,
+                    CurrentPrice = 24m,
+                },
+            };
+            return View(auctions);
         }
 
         public ActionResult TempDataDemo()
